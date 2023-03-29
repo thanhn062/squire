9ee9e1003e2f74897e75b3c09897a01a3ad366e4
Thanh Ngo <510thanh.ngo@gmail.com>
2023-03-28 19:52:12 -0700

    base code

---
+++ app.js
@@ -0,0 +1,22 @@
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
