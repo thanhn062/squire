1f84a5cb0607468a970c3733b105818b4ff504dd
Thanh Ngo <510thanh.ngo@gmail.com>
2023-03-28 23:45:15 -0700

    added dynamic command files processing & command executing, and response

--- app.js
+++ app.js
@@ -1,34 +1,58 @@
-// const { REST, Routes } = require('discord.js');
-
-// const commands = [
-//   {
-//     name: 'ping',
-//     description: 'Replies with Pong!',
-//   },
-// ];
-
-// TOKEN = "MTA5MDQ2ODA0ODU2MzA5MzUwNA.G1Ld-b.svL1XvZe-M2zKrVtevhWqZAtcDSF6EgbO9wJIU";
-// const rest = new REST({ version: '10' }).setToken(TOKEN);
-
-// (async () => {
-//   try {
-//     console.log('Started refreshing application (/) commands.');
-
-//     await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-
-//     console.log('Successfully reloaded application (/) commands.');
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })();
+// dependencies
 require('dotenv').config(); //initialize dotenv
-const Discord = require('discord.js'); //import discord.js
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
-const client = new Discord.Client(); //create new client
+// configuration
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+// dynamic command files
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
 
-//make sure this line is the last line
-client.login(process.env.CLIENT_TOKEN); //login bot using token
+// Client login
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.CLIENT_TOKEN);
