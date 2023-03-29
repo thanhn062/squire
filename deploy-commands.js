92746b4169d7b0f85e556a1d5836a82430202aee
Thanh Ngo <510thanh.ngo@gmail.com>
2023-03-28 23:45:58 -0700

    register slash commands to discord bot client

---
+++ deploy-commands.js
@@ -0,0 +1,50 @@
+const { REST, Routes } = require('discord.js');
+// const { clientId, guildId, token } = require('./config.json');
+require('dotenv').config(); //initialize dotenv
+
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+// dynamic command files
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+	    commands.push(command.data.toJSON());
+    }
+}
+// for (const file of commandFiles) {
+// 	const command = require(`./commands/${file}`);
+// 	commands.push(command.data.toJSON());
+// }
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(process.env.CLIENT_TOKEN);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(process.env.clientId, process.env.guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
