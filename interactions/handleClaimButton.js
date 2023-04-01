793cd93e37742eac128908754682a677dabe6e71
Thanh Ngo <510thanh.ngo@gmail.com>
2023-03-31 23:07:54 -0700

    added DM ticket with resolve & unclaim for guardian inbox & edit message content for student

--- interactions/handleClaimButton.js
+++ interactions/handleClaimButton.js
@@ -1,4 +1,5 @@
-const {ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
+const {ActionRowBuilder, ButtonBuilder, ButtonStyle, Attachment } = require('discord.js')
+const dmEmbedBuilder = require('../resources/embeds-msg/help-ticket-DM.js');
 
 const handleClaimButton = async (interaction) => {
 
@@ -12,20 +13,35 @@ const handleClaimButton = async (interaction) => {
     }
     else {
         console.log("Ticket claimed! Sending messages to student and guardian")
-        const messageContent = "this is a test";
 
         // DM Guardian
+        // get message details and build the embeds prop
+        const isoString = interaction.message.embeds[0].data.timestamp;
+        const date = new Date(isoString);
+        let embedProps = {
+            title: interaction.message.embeds[0].data.title,
+            timestamp: date,
+            footer: interaction.message.embeds[0].data.footer.text,
+            userID: interaction.message.embeds[0].data.fields[0].value,
+            description: interaction.message.embeds[0].data.description,
+            attachment: interaction.message.embeds[0].data.thumbnail.url,
+            msgURL: interaction.message.id
+        }
+        // build the embeds
+        let guardian_ticket = dmEmbedBuilder(embedProps)
+        // sends the embeds to guardian by DM
         const member = await interaction.guild.members.fetch(guardian_discord_id);
         const user = await member.user.fetch();
         const dmChannel = await user.createDM();
-        dmChannel.send({ content: messageContent });
+        dmChannel.send({content: '```Thank you for helping out our student!\nPlease reach out to them via DM, and then come back here to update the status of the ticket once the student has been helped.```',embeds: [guardian_ticket.embed], components: [guardian_ticket.buttons]});
 
         // DM Student
         const member2 = await interaction.guild.members.fetch(student_discord_id);
         const user2 = await member2.user.fetch();
         const dmChannel2 = await user2.createDM();
-        dmChannel2.send({ content: messageContent });
-        await interaction.reply({content: "Thanks for claiming this ticket! Please check your DM's for further instruction!", ephemeral: true})
+        dmChannel2.send({ content: `> __**Guardian:**__ <@${guardian_discord_id}>\n\`\`\`Has claimed your help request, and will be reaching out to you shortly.\nMake sure to check your DMs and Message Requests!\`\`\``});
+        await interaction.update({})
+        // await interaction.reply({content: "Thanks for claiming this ticket! Please check your DM's for further instruction!", ephemeral: true})
 
         let inProgressButton = new ActionRowBuilder()
         .addComponents(
