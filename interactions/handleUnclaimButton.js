129a72860a04c202efbb4d6cbf6f7d357ea04c7c
Thanh Ngo <510thanh.ngo@gmail.com>
2023-04-01 22:54:45 -0700

    added function to unclaim button inside guardian's DM

---
+++ interactions/handleUnclaimButton.js
@@ -0,0 +1,70 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+// when user click unclaim
+// build the original embeds with original button
+// update the ticket by msgID
+// delete message from guardian DM
+const handleUnclaimButton = async (interaction) => {
+    // extract message Id from the customId
+    const messageID = interaction.customId.split("-")[2]
+
+    // build embeds from ticket details on help-board
+    const isoString = interaction.message.embeds[0].data.timestamp;
+    const date = new Date(isoString);
+    let embedProps = {
+        title: interaction.message.embeds[0].data.title,
+        timestamp: date,
+        footer: interaction.message.embeds[0].data.footer.text,
+        userID: interaction.message.embeds[0].data.fields[0].value,
+        description: interaction.message.embeds[0].data.description,
+        attachment: interaction.message.embeds[0].data.thumbnail.url,
+    }
+    let ticket = ticketEmbedBuilder(embedProps)
+
+    // update ticket on help board
+    const channel = await interaction.client.channels.cache.get(process.env.helpBoardChannelId);
+    channel.messages.fetch(messageID)
+        .then(message => message.edit({
+            embeds: [ticket.embed],
+            components: [ticket.buttons]
+        }))
+        .catch(console.error)
+    
+    // delete ticket inside of guardian DM
+    const dm = await interaction.client.channels.fetch(interaction.channelId);
+    const message = await dm.messages.fetch(interaction.message.id);
+    message.delete().catch(console.error);
+}
+
+const ticketEmbedBuilder = (props) => {
+    let {title, timestamp, footer, userID, description, attachment} = props;
+    let ticket = new EmbedBuilder()
+        .setColor(0x2A9D8F)
+        .setAuthor({name: "Help Request"})
+        .setTitle(title)
+        .setDescription(description)
+        .addFields(
+            { name: 'Student:', value: userID, inline: true}
+        )
+        .setThumbnail(attachment)
+        .setTimestamp(timestamp)
+        .setFooter({ text: footer });
+    
+    let buttons = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                .setCustomId('button-claim')
+                .setLabel('Claim')
+                .setStyle(ButtonStyle.Success)
+                .setEmoji('✔️'),
+            new ButtonBuilder()
+                .setCustomId('button-deny')
+                .setLabel('Deny')
+                .setStyle(ButtonStyle.Danger)
+                .setEmoji('✖️')
+            );
+    
+    return {embed: ticket, buttons: buttons};
+}   
+
+module.exports = handleUnclaimButton
