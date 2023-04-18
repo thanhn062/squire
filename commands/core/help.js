d00b61a06d5f3d0b69f8dee5140ab51a6522ee3f
Thanh Ngo <510thanh.ngo@gmail.com>
2023-04-17 22:00:25 -0700

    moved ticketEmbedBuilder for readability

--- commands/core/help.js
+++ commands/core/help.js
@@ -11,40 +11,6 @@ const execute = async (interaction) => {
     helpModalSubmit(interaction)
 }
 
-function ticketEmbedBuilder(props) {
-    let {userID, season, subject, language, project, description, attachment } = props
-
-    let defaultPic = "https://cdn.discordapp.com/attachments/997625130769469481/1091258869071740948/fppsmalllustrewall_textureproduct750x1000.jpg"
-    let url = (attachment == null) ? defaultPic : attachment.url
-    let embed = new EmbedBuilder()
-    .setColor(0x2A9D8F)
-    .setAuthor({name: "Help Request"})
-	.setTitle(`Project: ${project}`)
-	.setDescription(`${description}`)
-    .addFields(
-        { name: 'Student:', value: `<@${userID}>`, inline: true },
-	)
-    .setThumbnail(url)
-	.setTimestamp()
-	.setFooter({ text: `${season} • ${subject} • ${language}`});
-
-    let buttons = new ActionRowBuilder()
-        .addComponents(
-            new ButtonBuilder()
-                .setCustomId('button-claim')
-                .setLabel('Claim')
-                .setStyle(ButtonStyle.Success)
-                .setEmoji('✔️'),
-            new ButtonBuilder()
-                .setCustomId('button-deny')
-                .setLabel('Deny')
-                .setStyle(ButtonStyle.Danger)
-                .setEmoji('✖️')
-        )
-
-    return {embed: embed, buttons: buttons};
-}
-
 const helpModalSubmit = async interaction => {
 
     let embedProps = {
@@ -96,6 +62,40 @@ const helpModalBuilder = () => {
 
     // Show the modal to the user
     return modal
+}
+
+function ticketEmbedBuilder(props) {
+    let {userID, season, subject, language, project, description, attachment } = props
+
+    let defaultPic = "https://cdn.discordapp.com/attachments/997625130769469481/1091258869071740948/fppsmalllustrewall_textureproduct750x1000.jpg"
+    let url = (attachment == null) ? defaultPic : attachment.url
+    let embed = new EmbedBuilder()
+    .setColor(0x2A9D8F)
+    .setAuthor({name: "Help Request"})
+	.setTitle(`Project: ${project}`)
+	.setDescription(`${description}`)
+    .addFields(
+        { name: 'Student:', value: `<@${userID}>`, inline: true },
+	)
+    .setThumbnail(url)
+	.setTimestamp()
+	.setFooter({ text: `${season} • ${subject} • ${language}`});
+
+    let buttons = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+                .setCustomId('button-claim')
+                .setLabel('Claim')
+                .setStyle(ButtonStyle.Success)
+                .setEmoji('✔️'),
+            new ButtonBuilder()
+                .setCustomId('button-deny')
+                .setLabel('Deny')
+                .setStyle(ButtonStyle.Danger)
+                .setEmoji('✖️')
+        )
+
+    return {embed: embed, buttons: buttons};
 }
 
 module.exports = {data: slashHelp, execute: execute }
