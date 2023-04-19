f9e2c086809a7932d8626c4dfac31cadc12dc038
Thanh Ngo <510thanh.ngo@gmail.com>
2023-04-18 19:00:01 -0700

    add test commands

---
+++ commands/core/test_help.js
@@ -0,0 +1,56 @@
+const testSlashHelp = require('../../resources/commands-form/test_help.js')
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+require('dotenv').config();
+
+const execute = async (interaction) => {
+    let embedProps = {
+        season: interaction.options.getString('season'),
+        subject: interaction.options.getString('subject'),
+        language: interaction.options.getString('language'),
+        project: interaction.options.getString('project'),
+        description: interaction.options.getString('description'),
+        userID: "111622508104450048",
+    }
+    const channel = interaction.client.channels.cache.get(process.env.helpBoardChannelId);
+    let ticket = ticketEmbedBuilder(embedProps)
+    channel.send({embeds: [ticket.embed], components: [ticket.buttons]})
+    await interaction.reply({ content: `Your request has been sent to the guardians!`, ephemeral: true});
+
+}
+
+function ticketEmbedBuilder(props) {
+    let {userID, season, subject, language, project, description, attachment } = props
+
+    let defaultPic = "https://cdn.discordapp.com/attachments/997625130769469481/1091258869071740948/fppsmalllustrewall_textureproduct750x1000.jpg"
+    let url = (attachment == null) ? defaultPic : attachment.url
+    let embed = new EmbedBuilder()
+    .setColor(0x2A9D8F)
+    .setAuthor({name: "Help Request"})
+	.setTitle(`Project: ${project}`)
+	.setDescription(`${description}`)
+    .addFields(
+        { name: 'Student:', value: `<@${userID}>`, inline: true },
+	)
+    .setThumbnail(url)
+	.setTimestamp()
+	.setFooter({ text: `${season} • ${subject} • ${language}`});
+
+    let buttons = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+                .setCustomId('button-claim')
+                .setLabel('Claim')
+                .setStyle(ButtonStyle.Success)
+                .setEmoji('✔️'),
+            new ButtonBuilder()
+                .setCustomId('button-deny')
+                .setLabel('Deny')
+                .setStyle(ButtonStyle.Danger)
+                .setEmoji('✖️')
+        )
+
+    return {embed: embed, buttons: buttons};
+}
+
+module.exports = {data: testSlashHelp, execute: execute}
